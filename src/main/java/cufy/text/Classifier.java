6ec4f1e690adb10754459bae4578d2a42d07c1af
LSafer <lsaferse@gmail.com>
2020-04-14 22:58:26 +0300

    renamed ClassifyArguments -> ClassifyToken
    - see the class for more details

--- src/main/java/cufy/text/Classifier.java
+++ src/main/java/cufy/text/Classifier.java
@@ -17,8 +17,11 @@ package cufy.text;
 
 import cufy.lang.Clazz;
 
+import java.io.IOError;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.util.Objects;
 
 /**
  * A class that can classify sequences from a reader. With just a simple gate method (for the caller).
@@ -29,43 +32,47 @@ import java.io.Reader;
  */
 public interface Classifier {
 	/**
-	 * Return the proper clazz for the text in the given 'input'.
+	 * Get the clazz of the text given.
 	 *
-	 * @param input  the input to read from
-	 * @param output the initial output instance
-	 * @param <O>    the component-type of the clazz returned
-	 * @return the proper clazz for the text in the given reader
-	 * @throws NullPointerException if the given 'input' is null
-	 * @throws IOException          if any I/O exception occurs
-	 * @throws ClassifyException    if any classifying exception occurs
+	 * @param text to be classified
+	 * @param <T>  the type of the returned clazz
+	 * @return the clazz of the given text
+	 * @throws NullPointerException if the given 'text' is null
+	 * @throws ClassifyException    if any classification exception occurs
 	 */
-	default <O> Clazz<O> classify(Reader input, Clazz output) throws IOException {
-		return this.classify(new ClassifyArguments<>(input, output));
+	default <T> Clazz<T> classify(CharSequence text) {
+		Objects.requireNonNull(text, "text");
+		try {
+			return this.classify(new ClassifyToken<>(new StringReader(text.toString()), null));
+		} catch (IOException e) {
+			throw new IOError(e);
+		}
 	}
 
 	/**
-	 * Return the proper clazz for the text in the given 'input'.
+	 * Get the clazz of the text on the given reader.
 	 *
-	 * @param input the input to read from
-	 * @param <O>   the component-type of the clazz returned
-	 * @return the proper clazz for the text in the given reader
-	 * @throws NullPointerException if the given 'input' is null
-	 * @throws IOException          if any I/O exception occurs
-	 * @throws ClassifyException    if any classifying exception occurs
+	 * @param reader to classify the text that is in it
+	 * @param <T>    the type of the returned clazz
+	 * @return the clazz of the text on the given reader
+	 * @throws IOException          if any IO exception occurs
+	 * @throws ClassifyException    if any classification exception occurs
+	 * @throws NullPointerException if the given 'reader' is null
 	 */
-	default <O> Clazz<O> classify(Reader input) throws IOException {
-		return this.classify(new ClassifyArguments<>(input));
+	default <T> Clazz<T> classify(Reader reader) throws IOException {
+		Objects.requireNonNull(reader, "reader");
+		return this.classify(new ClassifyToken<>(reader, null));
 	}
 
 	/**
-	 * Set the {@link ClassifyArguments#output} on the given arguments to the proper clazz for the text in the {@link ClassifyArguments#input}.
+	 * Set the {@link ClassifyToken#output} on the given token to the proper clazz for the text in the {@link ClassifyToken#input}.
 	 *
-	 * @param arguments the classifying instance that holds the variables of this classification
-	 * @param <O>       the component-type of the clazz returned
+	 * @param token the classifying instance that holds the variables of this classification
+	 * @param <T>   the component-type of the clazz returned
 	 * @return the proper clazz for the text in the given reader
 	 * @throws IOException          if any I/O exception occurs
-	 * @throws NullPointerException if the given 'arguments' is null
+	 * @throws NullPointerException if the given 'token' is null
 	 * @throws ClassifyException    if any classifying exception occurs
 	 */
-	<O> Clazz<O> classify(ClassifyArguments<?, O> arguments) throws IOException;
+	<T> Clazz<T> classify(ClassifyToken<T> token) throws IOException;
 }
---
+++ src/main/java/cufy/text/ClassifyToken.java
@@ -0,0 +1,112 @@
+/*
+ *	Copyright 2020 Cufy
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *	    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package cufy.text;
+
+import cufy.lang.Clazz;
+
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * A classification instance that holds the variables of a classification.
+ *
+ * @param <T> the type of the output
+ * @author lsafer
+ * @version 0.1.2
+ * @since 30-Mar-2020
+ */
+public class ClassifyToken<T> {
+	/**
+	 * A table of data to be copied from this token to it's sub-tokens.
+	 */
+	final public Map data;
+	/**
+	 * A table of data globally shared across this token and it's sub-tokens.
+	 */
+	final public Map data_global;
+	/**
+	 * The depth of this token form the first parent.
+	 */
+	final public int depth;
+	/**
+	 * The reader to read input from.
+	 *
+	 * @apiNote return it to it's original position after using it
+	 */
+	final public Reader input;
+	/**
+	 * The classifying token for the formatting that required initializing this token.
+	 */
+	final public ClassifyToken parent;
+	/**
+	 * The output of the classification. (could be changed several times!)
+	 */
+	public Clazz<T> output;
+
+	/**
+	 * Construct a new classifying token instance.
+	 *
+	 * @param input  the input to read from
+	 * @param output the initial output instance
+	 * @throws NullPointerException if the given 'input' is null
+	 */
+	public ClassifyToken(Reader input, Clazz<T> output) {
+		this(null, input, output);
+	}
+
+	/**
+	 * Construct a new classifying token instance.
+	 *
+	 * @param parent the parent token
+	 * @param input  the input to read from
+	 * @param output the initial output instance
+	 * @throws NullPointerException if the given 'input' is null
+	 */
+	protected ClassifyToken(ClassifyToken parent, Reader input, Clazz<T> output) {
+		Objects.requireNonNull(input, "input");
+
+		//NO RECURSION DETECTION
+
+		this.parent = parent;
+		this.input = input;
+		this.output = output;
+
+		if (parent == null) {
+			this.depth = 0;
+			this.data = new HashMap();
+			this.data_global = new HashMap();
+		} else {
+			this.depth = parent.depth + 1;
+			this.data = new HashMap(parent.data);
+			this.data_global = parent.data_global;
+		}
+	}
+
+	/**
+	 * Get a sub token of this token with the given parameters.
+	 *
+	 * @param input  the input to read from
+	 * @param output the initial output instance
+	 * @param <U>    the type of the output in the sub token
+	 * @return a sub token of this token
+	 * @throws NullPointerException if the given 'input' is null
+	 */
+	public <U> ClassifyToken<U> subToken(Reader input, Clazz<U> output) {
+		return new ClassifyToken<>(this, input, output);
+	}
+}
