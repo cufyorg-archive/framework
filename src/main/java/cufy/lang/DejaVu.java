a9eb4b7118fc31575c326241d4c7ce5dca5970bc
LSafer <lsaferse@gmail.com>
2020-04-19 02:24:08 +0300

    Create DejaVu.java
    - kinda needed it!

---
+++ src/main/java/cufy/lang/DejaVu.java
@@ -0,0 +1,19 @@
+package cufy.lang;
+
+/**
+ * A class represents dejaVu.
+ *
+ * @author lsafer
+ * @version 0.1.3
+ * @since 03-Mar-2020
+ */
+final public class DejaVu {
+	/**
+	 * This is a representation class and should not be instanced.
+	 *
+	 * @throws AssertionError when called
+	 */
+	private DejaVu() {
+		throw new AssertionError("No instance for you!");
+	}
+}
