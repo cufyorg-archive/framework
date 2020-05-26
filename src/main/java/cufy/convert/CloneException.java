a902fe1b5d14b7ce063837d6ebf9c190f8bcaf92
LSafer <lsaferse@gmail.com>
2020-05-26 19:36:26 +0300

    Create CloneException.java
    Thrown when An attmpt to clone an object failed

---
+++ src/main/java/cufy/convert/CloneException.java
@@ -0,0 +1,64 @@
+package cufy.convert;
+
+/**
+ * Thrown to indicate that the code has attempted to clone an object which it can't be cloned.
+ *
+ * @author lsafer
+ * @version 0.1.3
+ * @since 26-May-2020
+ */
+public class CloneException extends ConvertException {
+	/**
+	 * Constructs a new class clone exception with null as its detail message. The cause is not initialized, and may subsequently be initialized
+	 * by a call to Throwable.initCause(java.lang.Throwable).
+	 */
+	public CloneException() {
+	}
+
+	/**
+	 * Constructs a new class clone exception with the specified detail message. The cause is not initialized, and may subsequently be
+	 * initialized by a call to Throwable.initCause(java.lang.Throwable).
+	 *
+	 * @param message the detail message. The detail message is saved for later retrieval by the Throwable.getMessage() method.
+	 */
+	public CloneException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a new class clone exception with the specified detail message and cause. Note that the detail message associated with cause is
+	 * not automatically incorporated in this class clone exception's detail message.
+	 *
+	 * @param message the detail message (which is saved for later retrieval by the Throwable.getMessage() method).
+	 * @param cause   the cause (which is saved for later retrieval by the Throwable.getCause() method). (A null value is permitted, and indicates
+	 *                that the cause is nonexistent or unknown.)
+	 */
+	public CloneException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a new class clone exception with the specified cause and a detail message of (cause==null ? null : cause.toString()) (which
+	 * typically contains the class and detail message of cause). This constructor is useful for class conversion exceptions that are little more than
+	 * wrappers for other throwables.
+	 *
+	 * @param cause the cause (which is saved for later retrieval by the Throwable.getCause() method). (A null value is permitted, and indicates that
+	 *              the cause is nonexistent or unknown.)
+	 */
+	public CloneException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Constructs a new class clone exception with the specified detail message, cause, suppression enabled or disabled, and writable stack trace
+	 * enabled or disabled.
+	 *
+	 * @param message            the detail message.
+	 * @param cause              the cause. (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+	 * @param enableSuppression  whether or not suppression is enabled or disabled
+	 * @param writableStackTrace whether or not the stack trace should be writable
+	 */
+	protected CloneException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
