6dbae28e9a713583edbc8fe44bf64839bc708cf2
LSafer <lsaferse@gmail.com>
2020-06-28 14:06:10 +0300

    Final touches to the `beans` package info, Next time in the bug fixes! (إن شاء الله).

--- src/main/java/cufy/beans/package-info.java
+++ src/main/java/cufy/beans/package-info.java
@@ -14,13 +14,13 @@
  *	limitations under the License.
  */
 /**
- * Objects have fields. Those fields can't be maneged easily when the class of the object is unknown (without reflection). The beans fixes that by
- * treating the object as a map and the fields of that object will work as if they're the entries of the map. keeping the reflection part hidden from
- * the user.
+ * Objects have fields. Those fields can't be maneged easily when the class of the object is unknown (without
+ * reflection). The beans establish that by treating the object as a map, and the fields of that object will work as if
+ * they're the entries of the map. keeping the reflection part hidden from the user.
  *
- * @author lsafer
+ * @author LSafer
  * @version 0.1.5
- * @see <a href="https://framework.cufy.org/site/beans">framework.cufy.org/site/beans</a>
- * @since 11-Jun-2019
+ * @see <a href="https://framework.cufy.org/beans">framework.cufy.org/beans</a>
+ * @since 0.0.a ~2019.06.11
  */
 package cufy.beans;
